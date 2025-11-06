1528dfe5e3510eb8ad3153757b066bec219b2855
Giulia Martini <martini.giulia94@gmail.com>
2025-11-06 11:26:49 +0100

    Improve viz

--- data_utils.py
+++ data_utils.py
@@ -85,7 +85,7 @@ def plot_trend_points(df):
     fig, ax = plt.subplots()
 
     for name in df.Name.unique():
-        ax.plot(df[df.Name == name].Date, df[df.Name == name].cumulative_points, label=name, linestyle="dashed")
+        ax.plot(df[df.Name == name].Date, df[df.Name == name].cumulative_points, label=name, linestyle="dashed", marker='o')
 
     ax.set_xlabel("Giornata")
     ax.set_ylabel("Punti")
